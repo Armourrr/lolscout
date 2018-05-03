6908cead2dbd548ab3c9866f2c172cccb16a4bb3
Armourrr <u1360459@unimail.hud.ac.uk>
2018-05-04 00:08:32 +0100

    REnamed stats to calcpermin, test methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,60 +1,64 @@
-import Services.StatsAggregation;
+import net.rithms.riot.api.ApiConfig;
+import net.rithms.riot.api.RiotApi;
+import net.rithms.riot.api.endpoints.match.dto.*;
+import net.rithms.riot.api.endpoints.summoner.dto.Summoner;
+import net.rithms.riot.constant.Platform;
+import services.CalcPerMin;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
-import net.rithms.riot.api.ApiConfig;
-import net.rithms.riot.api.RiotApi;
 import net.rithms.riot.api.RiotApiException;
-import net.rithms.riot.api.endpoints.match.dto.Match;
-import net.rithms.riot.api.endpoints.match.dto.MatchList;
-import net.rithms.riot.api.endpoints.match.dto.MatchReference;
-import net.rithms.riot.api.endpoints.static_data.dto.Champion;
-import net.rithms.riot.api.endpoints.summoner.dto.Summoner;
-import net.rithms.riot.constant.Platform;
-import javafx.*;
 
 import java.io.IOException;
-import java.util.Calendar;
+import java.util.List;
 
 public class Main extends Application {
-
-
     public static void main(String[] args) throws RiotApiException {
-        StatsAggregation stats = new StatsAggregation();
-        System.out.println("Dmg per gold: " + stats.calculateDmgPerGold(41642, 19411));
+        CalcPerMin stats = new CalcPerMin();
 
-//        ApiConfig config = new ApiConfig().setKey("RGAPI-f89967bf-f46a-4514-a91d-198eaddbda62");
-//        RiotApi api = new RiotApi(config);
-//
-//
-//        // First we need to request the summoner because we will need it's account ID
-//        Summoner summoner = api.getSummonerByName(Platform.EUW, "Armour");
-//
-//        // Then we can use the account ID to request the summoner's match list
-//
-//        MatchList matchList = api.getMatchListByAccountId(Platform.EUW, summoner.getAccountId());
-//
-//
-//        System.out.println("Total Games in requested match list: " + matchList.getTotalGames());
-//        Match match = api.getMatch(Platform.EUW, matchList.getMatches().get(0).getGameId());
-//        long duration = match.getGameDuration();
-//        int goldSummoner0 = match.getParticipants().get(0).getStats().getGoldEarned();
-//        int wardsSummoner0 = match.getParticipants().get(0).getStats().getWardsPlaced();
-//
-//        StatsAggregation stats = new StatsAggregation();
-//        System.out.println("Trynd gold" + goldSummoner0);
-//
-//        System.out.println("Trynd GPM: " + stats.calculateGoldPerMinute(goldSummoner0,duration));
-//        System.out.println("Trynd WPM: " + stats.calculateWardsPerMinute(wardsSummoner0, duration));
+        ApiConfig config = new ApiConfig().setKey("RGAPI-f89967bf-f46a-4514-a91d-198eaddbda62");
+        RiotApi api = new RiotApi(config);
+
+        // First we need to request the summoner because we will need it's account ID
+        Summoner summoner = api.getSummonerByName(Platform.EUW, "Armour");
+        api.getSummoner(Platform.EUW,summoner.getId());
+
+        // Then we can use the account ID to request the summoner's match list
+
+        MatchList matchList = api.getMatchListByAccountId(Platform.EUW, summoner.getAccountId());
+
+
+        System.out.println("Total Games in requested match list: " + matchList.getTotalGames());
+        Match match = api.getMatch(Platform.EUW, matchList.getMatches().get(0).getGameId());
+        long duration = match.getGameDuration();
+        ParticipantStats summStats;
+        Summoner summ;
+        List<ParticipantIdentity> list = match.getParticipantIdentities();
+        int i = 0;
+        for (Participant player:match.getParticipants()) {
+            summ = api.getSummoner(Platform.EUW, list.get(i).getPlayer().getSummonerId());
+            summStats = player.getStats();
+            int gold = summStats.getGoldEarned();
+            int wards = summStats.getWardsPlaced();
+            int dmg = (int)summStats.getTotalDamageDealtToChampions();
+            int wardsCleared = summStats.getWardsKilled();
+            int creepScore = summStats.getTotalMinionsKilled();
+            System.out.println("---------------------------------------------");
+            System.out.println("Summoner: " + summ.getName());
+            System.out.println("    DPM: " + stats.calculateDamagePerMinute(dmg, duration));
+            System.out.println("    WPM: " + stats.calculateWardsPerMinute(wards, duration));
+            System.out.println("    WCPM: " + stats.calculateWardsClearedPerMinute(wardsCleared, duration));
+            System.out.println("    CSPM: " + stats.calculateCreepsPerMinute(creepScore, duration));
+            System.out.println("    GPM: " + stats.calculateGoldPerMinute(gold,duration));
+            System.out.println("    Dmg per gold: " + stats.calculateDmgPerGold(dmg, gold));
+            i++;
+
+        }
+
 
 
         Application.launch(args);
@@ -83,28 +87,4 @@ public class Main extends Application {
         });
         primaryStage.show();
     }
-//        primaryStage.setTitle("LoLScout");
-//        StackPane root = new StackPane();
-//        Scene scene = new Scene(root, 1280, 720);
-//
-//        Rectangle rec = new Rectangle(1280,720);
-//        rec.setFill(Color.BLACK);
-//        root.getChildren().add(rec);
-//
-//        File dir = new File(Config.DIR_SPLASH);
-//        File[] files = dir.listFiles();
-//
-//        Random rand = new Random();
-//
-//        File file = files[rand.nextInt(files.length)];
-//
-//        Image image = new Image(file.toURI().toString());
-//        ImageView iv = new ImageView(image);
-//        iv.setFitWidth(1280);
-//        iv.setFitHeight(720);
-//        iv.setOpacity(0.3);
-//
-//        root.getChildren().add(iv);
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
 }
